cc450e1841dd2af0e3aa0dc1b30b5b8056ece7bb
dummy <tele1841@gmail.com>
2022-09-17 19:13:29 +0900

    로그인 페이지 추가, kakao react package 설치

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import KaKaoLogin from 'react-kakao-login';
+const Login = (prop: any) => {
+    // const kauthUrl=`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_KEY}&redirect_uri=${카카오에서 설정한 redirect URI}&response_type=code`
+}
